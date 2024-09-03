19b82c6e8f29720422ffb849d3feb317a3f57b99
Silas Justino <105759554+silasjustino@users.noreply.github.com>
2024-09-03 09:15:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -57,12 +57,51 @@ function changeTheme() {
     btnTheme.onclick = changeTheme
 })();
 
+function loadAnimation() {
+    // Iniciar as animações aqui
+    githubAvatar.style.visibility = "visible";
+    githubAvatar.style.animation = "avatar 0.8s both ease-out";
+    
+    const titleBadge = document.querySelector(".title-badge");
+    titleBadge.style.visibility = "visible";
+    titleBadge.style.animation = "opacity 0.5s ease-out both 0.7s";
+
+    const verifiedIcon = document.getElementById("verified");
+    verifiedIcon.style.visibility = "visible";
+    verifiedIcon.style.animation = "opacity 0.5s 1s ease-in both";
+
+    const description = document.querySelector(".description");
+    description.style.visibility = "visible";
+    description.style.animation = "description 0.5s 1.2s both ease-out";
+
+    const flag = document.getElementById("flag");
+    flag.style.visibility = "visible";
+    flag.style.animation = "opacity 0.5s 1.5s both linear, flag 1s 1.5s both cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+
+    const linkedIn = document.getElementById("linkedIn");
+    linkedIn.style.visibility = "visible";
+    linkedIn.style.animation = "opacity 0.5s 2s both linear, buttonsUp 1s 2s both cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+
+    const github = document.getElementById("github");
+    github.style.visibility = "visible";
+    github.style.animation = "opacity 0.5s 2.2s both linear, buttonsUp 1s 2.2s both cubic-bezier(0.175, 0.885, 0.32, 1.275";
+
+    const instagram = document.getElementById("instagram");
+    instagram.style.visibility = "visible";
+    instagram.style.animation = "opacity 0.5s 2.4s both linear, buttonsUp 1s 2.4s both cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+
+    const itch_io = document.getElementById("itch_io");
+    itch_io.style.visibility = "visible";
+    itch_io.style.animation = "opacity 0.5s 2.6s both linear, buttonsUp 1s 2.6s both cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+}
+
 function getAvatar() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data.avatar_url)
             githubAvatar.src = data.avatar_url
+            loadAnimation();
         })
         .catch(error => console.log(error))
 }
